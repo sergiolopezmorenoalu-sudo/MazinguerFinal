b1a26e29d4390f5b1fe4125740392984ba3fe00b
= <=>
2026-03-16 13:29:05 +0100

    Añadidos Arma, Mazinguer y ModuloBasico

---
+++ ModuloBasico.py
@@ -0,0 +1,37 @@
+# Example file showing a circle moving on screen
+import pygame
+import time
+import Mazinger
+
+# Añadir aquí las importaciones del armamento.
+#import Planeador
+#import PuñosFuera
+#import FuegoPecho
+#import RayosFotónicos
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((700, 800))
+running = True
+
+maz=Mazinger.Mazinger(screen)
+
+# Incorporar aquí el armamento con pares de líneas como éstos.
+#planeador= Planeador.Planeador()
+#maz.incorpora(planeador)
+#puños= PuñosFuera.PuñosFuera()
+#maz.incorpora(puños)
+#pecho= FuegoPecho.FuegoPecho()
+#maz.incorpora(pecho)
+#ojo= RayosFotónicos.RayosFotónicos()
+#maz.incorpora(ojo)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    maz.arm(screen)
+    time.sleep(0.5)
+    maz.desactiva(screen)
+    time.sleep(0.5)
+    maz.sigArm()
